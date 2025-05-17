a80997804fed15b574652bc29734a2c26d296630
alvin.f <alvin.f@sucofindo.co.id>
2025-05-17 11:58:10 +0700

    final oke stack ndvi evi doy

--- process_modis_indonesia.py
+++ process_modis_indonesia.py
@@ -57,7 +57,7 @@ def merge_images(image_paths, output_path):
     with rasterio.open(output_path, "w", **out_meta) as dest:
         dest.write(mosaic)
 
-def stack_and_clip(ndvi_path, evi_path, boundary_url, output_path):
+def stack_and_clip(ndvi_path, evi_path, doy_path, boundary_url, output_path):
     # Unduh GeoJSON langsung dari URL
     response = requests.get(boundary_url)
     response.raise_for_status()
@@ -69,10 +69,11 @@ def stack_and_clip(ndvi_path, evi_path, boundary_url, output_path):
         boundary = gpd.read_file(tmp_geojson.name)
         boundary_geom = boundary.geometry.values
 
-        with rasterio.open(ndvi_path) as ndvi, rasterio.open(evi_path) as evi:
+        with rasterio.open(ndvi_path) as ndvi, rasterio.open(evi_path) as evi, rasterio.open(doy_path) as doy:
             # Masking
             ndvi_array, ndvi_transform = mask(ndvi, boundary_geom, crop=True, filled=True, nodata=0, indexes=1)
             evi_array, evi_transform = mask(evi, boundary_geom, crop=True, filled=True, nodata=0, indexes=1)
+            doy_array, doy_transform = mask(doy, boundary_geom, crop=True, filled=True, nodata=0, indexes=1)
 
             profile = ndvi.profile
             profile.update({
@@ -86,13 +87,14 @@ def stack_and_clip(ndvi_path, evi_path, boundary_url, output_path):
             with rasterio.open(output_path, 'w', **profile) as dst:
                 dst.write(ndvi_array, 1)
                 dst.write(evi_array, 2)
+                dst.write(doy_array, 3)
 
 if __name__ == "__main__":
     download_folder = './modis_data'
     boundary_geojson_url = 'https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_IDN_0.json'
     os.makedirs(download_folder, exist_ok=True)
 
-    tiles = ['h27v08', 'h28v08', 'h28v09', 'h29v08', 'h29v09']  # Cakupan wilayah Indonesia
+    tiles = ['h27v08', 'h28v08', 'h28v09', 'h29v08', 'h29v09', 'h30v08', 'h30v09']  # Cakupan wilayah Indonesia
     dates = '2025-01-01'  # Sesuaikan tanggal
 
     # Unduh MODIS Data
@@ -102,26 +104,35 @@ if __name__ == "__main__":
 
     ndvi_files = []
     evi_files = []
+    doy_files = []
 
     for hdf in hdf_files:
         basename = os.path.splitext(os.path.basename(hdf))[0]
-        ndvi_path = os.path.join(download_folder, f'{basename}_ndvi.tif')
-        evi_path = os.path.join(download_folder, f'{basename}_evi.tif')
+        ndvi_path = os.path.join(download_folder, f'{basename}_ndvi')
+        evi_path = os.path.join(download_folder, f'{basename}_evi')
+        doy_path = os.path.join(download_folder, f'{basename}_doy')
 
         # Ekstraksi band NDVI & EVI dari MOD13Q1
-        extract_band_modis_gdal(hdf, '250m 16 days NDVI', ndvi_path)
-        extract_band_modis_gdal(hdf, '250m 16 days EVI', evi_path)
+        extract_band_modis_gdal(hdf, '( 1 0 )', ndvi_path) # ndvi
+        extract_band_modis_gdal(hdf, '( 0 1 )', evi_path) # evi
+        extract_band_modis_gdal(hdf, '( 0 0 0 0 0 0 0 0 0 0 1 )', doy_path) # doy
 
-        ndvi_files.append(ndvi_path)
-        evi_files.append(evi_path)
+
+        ndvi_files.append(f'{ndvi_path}.tif')
+        evi_files.append(f'{evi_path}.tif')
+        doy_files.append(f'{doy_path}.tif')
 
     merged_ndvi = os.path.join(download_folder, 'merged_ndvi.tif')
     merged_evi = os.path.join(download_folder, 'merged_evi.tif')
+    merged_doy = os.path.join(download_folder, 'merged_doy.tif')
+
 
     merge_images(ndvi_files, merged_ndvi)
     merge_images(evi_files, merged_evi)
+    merge_images(doy_files, merged_doy)
 
-    stacked_output = os.path.join(download_folder, 'stacked_ndvi_evi_indonesia.tif')
-    stack_and_clip(merged_ndvi, merged_evi, boundary_geojson_url, stacked_output)
 
-    print("✅ Proses selesai. Output:", stacked_output)
+    stacked_output = os.path.join(download_folder, 'stacked_ndvi_evi_doy_indonesia.tif')
+    stack_and_clip(merged_ndvi, merged_evi, merged_doy, boundary_geojson_url, stacked_output)
+
+    print("✅ Proses selesai. Output:", merged_ndvi, merged_evi, merged_doy, stacked_output)
